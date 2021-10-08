a8c854e02704afe40499175f59a94d09fe412947
dsubmarineb <genechoi@utexas.edu>
2021-10-07 23:47:47 -0500

    some tests with callback function

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\genec\ProgrammingPractice\.venv
+set VIRTUAL_ENV=C:\Users\genec\Python_Programs\ProgrammingPractice\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
